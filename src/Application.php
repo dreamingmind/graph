8f54f9fb71f3559ec3b4dd1270cc899e0387fd5d
dreamingmind <ddrake@dreamingmind.com>
2023-08-30 15:15:59 -0700

    add fixture factories

--- src/Application.php
+++ src/Application.php
@@ -61,6 +61,7 @@ class Application extends BaseApplication
          */
         if (Configure::read('debug')) {
             $this->addPlugin('DebugKit');
+            $this->addPlugin('CakephpFixtureFactories');
         }
 
         // Load more plugins here
