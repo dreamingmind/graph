c88a375526cb5bca86a99e57e98ae1c498783edf
dreamingmind <ddrake@dreamingmind.com>
2023-09-07 09:43:53 -0700

    Make browser tabs identify the project

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -14,7 +14,7 @@
  * @var \App\View\AppView $this
  */
 
-$cakeDescription = 'CakePHP: the rapid development php framework';
+$cakeDescription = 'Graph4 Plugin Development';
 ?>
 <!DOCTYPE html>
 <html>
